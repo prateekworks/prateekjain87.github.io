fb25c4d19331e7250e8806ffdfb7e6d5c57b502a
prateekjain87 <akijain058@gmail.com>
2022-02-10 23:25:57 +0530

    Added Flutter code changes

--- E-commerce-Complete-Flutter-UI/lib/screens/home/components/domestic_category.dart
+++ E-commerce-Complete-Flutter-UI/lib/screens/home/components/domestic_category.dart
@@ -1,19 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-int itemCount = 10;
+import 'package:shop_app/models/Product.dart';
+import 'package:shop_app/components/product_card.dart';
+
+int item = 3;
 class Domestic extends StatelessWidget {
   static String routeName = "/domestic";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Empty List Test')),
-      body: itemCount > 0
+      body: item > 0
           ? ListView.builder(
-        itemCount: itemCount,
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            title: Text('Item ${index + 1}'),
-          );
+        itemCount: item,
+        itemBuilder: (context, position) {
+          return ProductCard(product: demoProducts[item-1]);
+          // return Card(
+          //   child: Padding(
+          //     padding: const EdgeInsets.all(25.0),
+          //     child: Text(position.toString(), style: TextStyle(fontSize: 22.0),),
+          //   ),
+          //);
         },
       )
           : const Center(child: Text('No items')),
