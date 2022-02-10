4175f3d4ea6d594c5d514b305cae52657371ad5d
prateekjain87 <akijain058@gmail.com>
2022-02-10 23:09:21 +0530

    Added Flutter code changes

--- E-commerce-Complete-Flutter-UI/lib/screens/home/components/domestic_category.dart
+++ E-commerce-Complete-Flutter-UI/lib/screens/home/components/domestic_category.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
+int itemCount = 10;
 class Domestic extends StatelessWidget {
   static String routeName = "/domestic";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Sign In"),
-      ),
+      appBar: AppBar(title: const Text('Empty List Test')),
+      body: itemCount > 0
+          ? ListView.builder(
+        itemCount: itemCount,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text('Item ${index + 1}'),
+          );
+        },
+      )
+          : const Center(child: Text('No items')),
     );
   }
 }
