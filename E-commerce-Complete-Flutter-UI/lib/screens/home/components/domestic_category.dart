93f68d3f84155fa6d5d44efc60782f3a2e93dcfc
prateekjain87 <akijain058@gmail.com>
2022-02-10 23:36:22 +0530

    Added file

--- E-commerce-Complete-Flutter-UI/lib/screens/home/components/domestic_category.dart
+++ E-commerce-Complete-Flutter-UI/lib/screens/home/components/domestic_category.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shop_app/models/Product.dart';
 import 'package:shop_app/components/product_card.dart';
 
